96a642a8f1d003609f394ff2b31f6b952b31a357
dhruvrana2910 <87424047+dhruvrana2910@users.noreply.github.com>
2021-07-19 16:08:00 +0530

    Adding filename

--- aws-lambda-test.tf
+++ aws-lambda-test.tf
@@ -19,7 +19,7 @@ EOF
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  # filename      = "lambda_function_payload.zip"
+  filename      = "lambda_function_payload.zip"
   function_name = "lambda_function_name"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"

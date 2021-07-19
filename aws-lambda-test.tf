812f41a353aa6eac7b8e0cb679ec543294a4a4b3
dhruvrana2910 <87424047+dhruvrana2910@users.noreply.github.com>
2021-07-19 16:11:19 +0530

    Adding source code details in lambda

--- aws-lambda-test.tf
+++ aws-lambda-test.tf
@@ -26,8 +26,8 @@ resource "aws_lambda_function" "test_lambda" {
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  # source_code_hash = filebase64sha256("lambda_function_payload.zip")
+  source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
   runtime = "nodejs12.x"
 

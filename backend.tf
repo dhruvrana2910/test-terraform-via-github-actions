5bb821c26c7b5608473eb6269a17db1ba92f0130
dhruvrana2910 <87424047+dhruvrana2910@users.noreply.github.com>
2021-07-19 15:15:29 +0530

    region renamed

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "test-terraform-github-actions"
     key            = "test-env/terraform.tfstate"
-    region         = "eu-west-2"
+    region         = "us-west-2"
     dynamodb_table = "terraform_state"
   }
 }
